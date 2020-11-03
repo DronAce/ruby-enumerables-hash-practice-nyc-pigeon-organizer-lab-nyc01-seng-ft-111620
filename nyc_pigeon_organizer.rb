fca7cf4233d27809393d2006ff56323b4da56470
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-03 22:37:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_list = getNames(data)
+  names = []
+  hash = {}
+  data.map do |k1,kv|
+      kv.find do |k, v|
+          names << v
+      end
+  end
+  names.flatten!
+  names  = names.uniq
+  names.each do |x|
+      x = x.to_sym
+      hash[x] = {}
+  end
+  return hash
 end
-
-def getNames(data) {
-names = []
-hash = {}
-data.map do |k1,kv|
-    kv.find do |k, v|
-        names << v
-    end
-end
-names.flatten!
-names  = names.uniq
-names.each do |x|
-    x = x.to_sym
-    hash[x] = {}
-end
-return hash
-}
