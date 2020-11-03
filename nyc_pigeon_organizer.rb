15a38b2377e5000d92a4257bb772fb03a8526559
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-03 22:31:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,4 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  hash ={}
-  data.map { |k1, k2, v| [k1, k2.to_s, v.to_sym, ]}.to_h
-  hash = data.inject({}){|h, (k1, k2, v)| h[v][k1] = k2}
+
 end
