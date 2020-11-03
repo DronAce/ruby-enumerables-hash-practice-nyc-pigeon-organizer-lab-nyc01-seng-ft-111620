b4292f22c9678669e3e64975c35cab0f38231bde
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-03 22:33:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,17 @@ def nyc_pigeon_organizer(data)
 
 end
 
-def names(data) {
-
+def getNames(data) {
+names = []
+data.map do |k1,kv|
+    kv.find do |k, v|
+        names << v
+    end
+end
+names.flatten!
+names  = names.uniq
+names.each do |x|
+    x = x.to_sym
+    hash[x] = {}
+end
 }
