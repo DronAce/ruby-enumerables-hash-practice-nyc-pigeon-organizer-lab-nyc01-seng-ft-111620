8f32e41dab65f94563a30919000d8039cde1bf29
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-04 00:23:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     kv.each do |k, v|
       v.map do |x|
         hash[x] ||= {}
-        hash[x][k1] = []
+        hash[x][k1] =|| []
         hash[x][k1] << k.to_s
       end
     end
